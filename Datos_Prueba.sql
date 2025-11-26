ab8d1a7e2ee4f5e91764c1b098f574e4ef3f65db
OnionSlayer45 <pepecc_512@outlook.com>
2025-11-25 23:28:56 -0600

    Add files via upload

---
+++ Datos_Prueba.sql
@@ -0,0 +1,20 @@
+insert into producto(precio,descri,nombre,id) values (13,'Esta rico','Helado',4);
+insert into producto(precio,descri,nombre,id) values (20,'Esta rico','Galletas',5);
+insert into producto(precio,descri,nombre,id) values (46,'Esta rico','elote',6);
+insert into producto(precio,descri,nombre,id) values (60,'Esta rico','Refresco 6L',7);
+insert into producto(precio,descri,nombre,id) values (25,'Esta rico','Cacahuates',8);
+insert into producto(precio,descri,nombre,id) values (1,'Esta rico','Picafresa',9);
+insert into producto(precio,descri,nombre,id) values (100,'Esta rico','Taza',10);
+insert into producto(precio,descri,nombre,id) values (123,'Esta rico','Taza con logo',11);
+insert into producto(precio,descri,nombre,id) values (120,'Esta rico','Guantes',12);
+insert into producto(precio,descri,nombre,id) values (150,'Esta rico','Gorro',13);
+insert into producto(precio,descri,nombre,id) values (200,'Esta rico','Playera hombre',14);
+insert into producto(precio,descri,nombre,id) values (230,'Esta rico','Playera mujer',15);
+insert into producto(precio,descri,nombre,id) values (300,'Esta rico','Sudadera',16);
+
+
+
+
+commit;
+
+
