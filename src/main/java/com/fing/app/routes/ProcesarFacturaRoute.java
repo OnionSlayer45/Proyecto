87f401bceac784a34e0180855ee8e92c10a90c02
JC01001 <j.castillo.c099@gmail.com>
2025-11-26 22:28:39 -0600

    Update ProcesarFacturaRoute.java

--- src/main/java/com/fing/app/routes/ProcesarFacturaRoute.java
+++ src/main/java/com/fing/app/routes/ProcesarFacturaRoute.java
@@ -8,14 +8,13 @@ public class ProcesarFacturaRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        // Escuchamos el canal "direct:facturacion"
         from("direct:facturacion")
             .routeId("ProcesoFacturacion")
             
             // Log inicial
             .log("CAMEL: Recibida solicitud de facturación -> ${body}")
             
-            // Simulamos generación de PDF y Timbrado SAT
+            // Simulamos  PDF y Timbrado SAT
             .process(exchange -> {
                 System.out.println("Generando XML :)");
                 Thread.sleep(500); 
